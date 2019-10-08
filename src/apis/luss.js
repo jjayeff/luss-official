cefa5055885c65483728c32abe645267fe28bfec
Jeff <jeff@almond.group>
2019-10-08 15:11:49 +0700

    [Update] api url

--- src/apis/luss.js
+++ src/apis/luss.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL:
-    'https://cors-anywhere.herokuapp.com/http://ec2-54-206-120-221.ap-southeast-2.compute.amazonaws.com:3000'
+    'https://cors-anywhere.herokuapp.com/http://ec2-54-206-110-80.ap-southeast-2.compute.amazonaws.com:4001'
 });
